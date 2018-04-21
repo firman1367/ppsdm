20f9ef2f77a658e9262ff6a9ecd02ff0a932a3c8
firman1367 <firman1367@gmail.com>
2018-04-21 12:56:29 +0700

    origin

--- form/cari_provinsi.php
+++ form/cari_provinsi.php
@@ -243,7 +243,7 @@
                     format: '{point.y:.f}', // one decimal
                     y: 3, // 10 pixels down from the top
                     style: {
-                        fontSize: '15px',
+                        fontSize: '15spx',
                         fontFamily: 'Verdana, sans-serif'
                         }
                     }
